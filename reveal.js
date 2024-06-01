5b6fff9a52bdf108154a11e5cc6cbd706690da18
Vinicius Santiago <142439760+Vini048@users.noreply.github.com>
2024-05-31 21:16:26 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,81 @@
+window.revelar = ScrollReveal({reset:true})
+
+//topo do site
+revelar.reveal('.efeito-txt-topo',{
+    duration: 2000,
+    distance: '90px'
+})
+
+// titulos das sessões
+revelar.reveal('.titulo', {
+    duration: 2000,
+    distance: '90px',
+    delay:300
+})
+//cards
+revelar.reveal('.t1', {
+    duration: 2000,
+    distance: '90px',
+    delay:1000,
+    origin: 'left'
+})
+revelar.reveal('.t2', {
+    duration: 2000,
+    distance: '90px',
+    delay:2000,
+    origin: 'rigth'
+})
+revelar.reveal('.t3', {
+    duration: 2000,
+    distance: '90px',
+    delay:3000
+  
+})
+revelar.reveal('.t4', {
+    duration: 2000,
+    distance: '90px',
+    delay:4000,
+    origin: 'rigth'
+})
+revelar.reveal('.t5', {
+    duration: 2000,
+    distance: '90px',
+    delay:5000,
+    origin: 'left'
+})
+// fotos 
+revelar.reveal('.efeito-foto1',{
+    duration: 2000,
+    distance: '90px',
+    delay:1000,
+    origin:'left'
+})
+revelar.reveal('.efeito-foto2',{
+    duration: 2000,
+    distance: '90px',
+    delay:2000,
+})
+revelar.reveal('.efeito-foto3',{
+    duration: 2000,
+    distance: '90px',
+    delay:2250,
+    origin:'left'
+})
+revelar.reveal('.efeito-foto4',{
+    duration: 2000,
+    distance: '90px',
+    delay:1000,
+    origin:'rigth'
+})
+revelar.reveal('.efeito-foto5',{
+    duration: 2000,
+    distance: '90px',
+    delay:2000,
+    origin:'left'
+})
+//LOGIN
+revelar.reveal('.login',{
+    duration: 2000,
+    distance: '90px',
+    delay:2000
+})
